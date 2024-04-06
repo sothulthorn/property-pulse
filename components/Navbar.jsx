6ab5d98170566942c3effee7c14f699623359836
sothulthorn <thornsothul@gmail.com>
2024-04-06 21:16:01 +0900

    Sign Out and Protect Routes

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -191,6 +191,7 @@ const Navbar = () => {
                       role="menuitem"
                       tabIndex="-1"
                       id="user-menu-item-0"
+                      onClick={() => setIsProfileMenuOpen(false)}
                     >
                       Your Profile
                     </Link>
@@ -204,6 +205,10 @@ const Navbar = () => {
                       Saved Properties
                     </Link>
                     <button
+                      onClick={() => {
+                        setIsProfileMenuOpen(false);
+                        signOut();
+                      }}
                       className="block px-4 py-2 text-sm text-gray-700"
                       role="menuitem"
                       tabIndex="-1"

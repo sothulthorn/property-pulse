d0296103c443ecbf5d760498521b38ea7062a399
sothulthorn <thornsothul@gmail.com>
2024-04-06 21:05:20 +0900

    Save user to database and session

--- utils/authOptions.js
+++ utils/authOptions.js
@@ -1,3 +1,5 @@
+import connectDB from '@/config/database';
+import User from '@/models/User';
 import GoogleProvider from 'next-auth/providers/google';
 
 export const authOptions = {
@@ -18,16 +20,37 @@ export const authOptions = {
     // Invoke on successful signin
     async signIn({ profile }) {
       // 1. Connect to database
+      await connectDB();
+
       // 2. Check if the user exists
+      const userExists = await User.findOne({ email: profile.email });
+
       // 3. If not, then add user to database
+      if (!userExists) {
+        // Truncate username if too long
+        const username = profile.name.slice(0, 20);
+
+        await User.create({
+          email: profile.email,
+          username,
+          image: profile.picture,
+        });
+      }
+
       // 4. Return true to allow sign in
+      return true;
     },
 
     // Midifies the session object
     async session({ session }) {
       // 1. Get user from the database
+      const user = await User.findOne({ email: session.user.email });
+
       // 2. Assign the user id to the session
+      session.user.id = user._id.toString();
+
       // 3. Return session
+      return session;
     },
   },
 };

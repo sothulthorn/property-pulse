9cd5db4f773db9439f7fbcdb1de817a8c736f2ea
sothulthorn <thornsothul@gmail.com>
2024-04-06 15:15:19 +0900

    Home Component

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import Hero from '@/components/Hero';
+import InfoBoxes from '@/components/InfoBoxes';
+import Footer from '@/components/Footer';
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <>
+      <Hero />
+      <InfoBoxes />
+      <Footer />
+    </>
+  );
 };
 
 export default HomePage;

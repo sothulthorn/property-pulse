2ed025ca18ab0d37dadccdb61704c6b4fcada0e8
sothulthorn <thornsothul@gmail.com>
2024-04-06 17:55:20 +0900

    Fetch Single Property

---
+++ app/properties/[id]/page.jsx
@@ -0,0 +1,34 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { useParams } from 'next/navigation';
+import { fetchProperty } from '@/utils/requests';
+
+const PropertyPage = () => {
+  const { id } = useParams();
+  const [property, setProperty] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchPropertyData = async () => {
+      if (!id) return;
+
+      try {
+        const property = await fetchProperty(id);
+        setProperty(property);
+      } catch (error) {
+        console.error('Error fetching property:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    if (property === null) {
+      fetchPropertyData();
+    }
+  }, [id, property]);
+
+  return <div>{id}</div>;
+};
+
+export default PropertyPage;
--- utils/requests.js
+++ utils/requests.js
@@ -1,5 +1,6 @@
 const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null;
 
+// Fetch All Properties
 async function fetchProperties() {
   try {
     // Handle the case where the domain is not available yet
@@ -21,4 +22,26 @@ async function fetchProperties() {
   }
 }
 
-export { fetchProperties };
+// Fetch Single Property
+async function fetchProperty(id) {
+  try {
+    // Handle the case where the domain is not available yet
+    if (!apiDomain) {
+      return null;
+    }
+
+    const res = await fetch(`${apiDomain}/properties/${id}`);
+
+    if (!res.ok) {
+      throw new Error('Failed to fetch data');
+    }
+
+    const data = await res.json();
+    return data.message;
+  } catch (error) {
+    console.error('Error fetching property:', error);
+    return null;
+  }
+}
+
+export { fetchProperties, fetchProperty };

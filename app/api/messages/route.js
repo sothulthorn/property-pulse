6b46b9f10acd0c25f0a4ff6dd04e078af1e8e3fe
sothulthorn <thornsothul@gmail.com>
2024-04-10 12:42:47 +0900

    Fetch Messages

--- app/api/messages/route.js
+++ app/api/messages/route.js
@@ -2,6 +2,32 @@ import connectDB from '@/config/database';
 import Message from '@/models/Message';
 import { getSessionUser } from '@/utils/getSessionUser';
 
+// GET /api/messages
+export const GET = async (request) => {
+  try {
+    await connectDB();
+
+    const sessionUser = await getSessionUser();
+
+    if (!sessionUser || !sessionUser.user) {
+      return new Response(JSON.stringify('User Id is required'), {
+        status: 401,
+      });
+    }
+
+    const { userId } = sessionUser;
+
+    const messages = await Message.find({ recipient: userId })
+      .populate('sender', 'name')
+      .populate('property', 'title');
+
+    return new Response(JSON.stringify(messages), { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return new Response('Something went wrong', { status: 500 });
+  }
+};
+
 // POST /api/messages
 export const POST = async (request) => {
   try {

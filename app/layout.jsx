a112680d5c13edd8002546240f5b67b7aed98ed9
sothulthorn <thornsothul@gmail.com>
2024-04-10 14:43:37 +0900

    Photoswipe Lightbox

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,6 +6,7 @@ import { GlobalProvider } from '@/context/GlobalContext';
 import { ToastContainer } from 'react-toastify';
 import '@/assets/styles/globals.css';
 import 'react-toastify/dist/ReactToastify.css';
+import 'photoswipe/dist/photoswipe.css';
 
 export const metadata = {
   title: 'Property Pulse | Find The Perfect Rental',
